9c280b6b57795c9c08caade067d9f79ae35a2c93
kishancjxLap <kishancjx@gmail.com>
2024-09-12 04:52:05 +0530

    Almost Everything is Working if used Correctly.. But BUGS are there which i will solve later.. such as if we keep pressing the numbers it will keep adding again and again.. and more bugs...

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
 console.log("Hello World");
+
+let previousNum = null;
+let currentNum = null;
+let currentOperator = "kundi";
+
+const buttons = document.querySelector(".buttons");
+const display = document.querySelector(".display");
+display.textContent = "0";
+
+buttons.addEventListener("click", (event) => {
+  // Checks if Numbers are pressed
+  if (event.target.className == "number") {
+    updateNumber(event.target.textContent);
+    console.log(event.target.textContent);
+    display.textContent = event.target.textContent;
+  }
+  // Checks if Operators are pressed
+  if (event.target.className == "operator") {
+    currentOperator = event.target.textContent;
+    console.log(event.target.textContent);
+    display.textContent = previousNum;
+  }
+
+  // Checks if other Buttons Press
+  if (event.target.className == "other") {
+    handleOtherButtons(event.target.textContent);
+  }
+  console.log(event.target.className);
+});
+
+function updateNumber(pressedNumber) {
+  // Check if it is the First Time
+  if (previousNum === null && currentNum === null)
+    previousNum = parseFloat(pressedNumber);
+  // Other than First time
+  else {
+    currentNum = parseFloat(pressedNumber);
+    calculate();
+  }
+}
+
+function calculate() {
+  if (currentOperator === "+") previousNum = previousNum + currentNum;
+  else if (currentOperator === "-") previousNum = previousNum - currentNum;
+  else if (currentOperator === "*") previousNum = previousNum * currentNum;
+  else if (currentOperator === "/") previousNum = previousNum / currentNum;
+  else if (currentOperator === "%") previousNum = previousNum % currentNum;
+
+  console.log(previousNum);
+}
+
+function handleOtherButtons(thatButton) {
+  if (thatButton === "AC") {
+    previousNum = null;
+    currentNum = null;
+    display.textContent = "0";
+  }
+
+  // other COde I will write Later;
+}
